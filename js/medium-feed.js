21ac854287fe31b1813dda45ec18744ef8debaff
Ernesto <ernestogn@gmail.com>
2018-01-29 18:51:05 -0600

    Blog Update

--- js/medium-feed.js
+++ js/medium-feed.js
@@ -1,7 +1,7 @@
 $(function () {
 	var $content = $('#jsonContent');
 	var data = {
-		rss_url: 'https://medium.jasonmdesign.com/feed'
+		rss_url: 'https://medium.com/feed/@ernestogarcian'
 	};
 	$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
 		if (response.status == 'ok') {
@@ -10,21 +10,20 @@ $(function () {
 				var visibleSm;
 				if(k < 3){
 					visibleSm = '';
-				 } else {
-					 visibleSm = ' visible-sm';
-				 }
-				output += '<div class="col-sm-6 col-md-4' + visibleSm + '">';
+				} else {
+					visibleSm = 'visible-sm';
+				}
+				output += '<div class="col-md-4 wp4' + visibleSm + '">';
 				output += '<div class="blog-post"><header>';
-				output += '<h4 class="date">' + $.format.date(item.pubDate, "dd<br>MMM") + "</h4>";
 				var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
 				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
 				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
 				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
 				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
-				output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="360px" height="240px"></div></header>';
-				output += '<div class="blog-content"><h4><a href="'+ item.link + '">' + item.title + '</a></h4>';
+				output += '<div class="overlay-effect effects clearfix"><div class="img"><img class="grayscale" src="' + src + '" alt="Blog Item"></div></div></header>';
+				output += '<div class="blog-content"><h2><a href="'+ item.link + '">' + item.title + '</a></h2>';
 				output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
-				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
+				var yourString = item.description.replace(/<img[^>]*>/g,"");
 				var maxLength = 120 // maximum number of characters to extract
 				//trim the string to the maximum length
 				var trimmedString = yourString.substr(0, maxLength);
