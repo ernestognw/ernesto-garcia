0c82e39d3a48c07177372671cf090e1af5e2177a
Ernesto <ernestogn@gmail.com>
2018-01-31 11:09:12 -0600

    Update

--- js/behance-feed.js
+++ js/behance-feed.js
@@ -1,106 +1,25 @@
+var apiKey = 'ycN420C1IQyzg3vaclQ5uXOMhx0toixh'; // Your API Key/Client ID. 
+// If you don't have one, go get it here: https://www.behance.net/dev/register
+var user = 'ramotion'; // This is your behance username
+var projectsPerRow = 3; // Select 4 or 6
+var projectsPerColumn = 2; // Select whatever you want
+
 $(function () {
-	var $content_zero = $('#jsonBehance-0');
-	var $content_one = $('#jsonBehance-1');
-	var $content_two = $('#jsonBehance-2');
-	var $content_three = $('#jsonBehance-3');
-	var $content_four = $('#jsonBehance-4');
-	var $content_five = $('#jsonBehance-5');
-	var $content_six = $('#jsonBehance-6');
-	var $content_seven = $('#jsonBehance-7');
 	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
+	    url: "https://api.behance.net/v2/users/" + user + "/projects?client_id=" + apiKey,
 	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
+	    success:
+	    function(results){
+	    	var projectsArray = new Array();
+	    	for (var i = 0 ; i < projectsPerRow*projectsPerColumn; i++) {
+		    	var project = results.projects[i];
 
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["0"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["0"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_zero.html(output);
-	    }
-	});
-	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
-	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
-
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["1"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["1"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_one.html(output);
-	    }
-	});
-	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
-	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
-
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["2"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["2"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_two.html(output);
-	    }
-	});
-	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
-	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
-
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["3"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["3"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_three.html(output);
-	    }
-	});
-	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
-	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
-
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["4"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["4"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_four.html(output);
-	    }
-	});
-	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
-	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
-
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["4"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["4"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_five.html(output);
-	    }
-	});
-	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
-	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
-
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["4"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["4"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_six.html(output);
-	    }
-	});
-	$.ajax({
-	    url: "https://api.behance.net/v2/users/ernesto-garcia/projects?client_id=ycN420C1IQyzg3vaclQ5uXOMhx0toixh",
-	    dataType: 'jsonp',
-	    success: function(results){
-	    	console.log(results.projects)
-	    	var output;
-
-	    	var project_one = results.projects["0"].name;
-	    	output = '<a href="' + results.projects["4"].url + '" data-lightbox="roadtrip" title="Project Eight - Lorem Ipsum"><img class="grayscale" src="' + results.projects["4"].covers[404] + '" alt="Portfolio Item"></a>';
-    		$content_seven.html(output);
+		    	var output;
+		    	output = '<div class="col-md-4 col-sm-6"><div class="overlay-effect effects clearfix"><div class="img"><a href="' + project.url + '" target="blank_" title="' + project.name + '"><div class="portfolio-img-hover"><div class="img-backdrop"></div><div class="description-container"><h3>' + project.name + '</h3></div></div><img class="grayscale" src="' + project.covers[404] + '" alt="Portfolio Item"></a></div></div></div>';
+		    	projectsArray.push(output);
+	    	}
+		    var $content = $('#behance-feed');
+		    $content.html(projectsArray);
 	    }
 	});
 });
